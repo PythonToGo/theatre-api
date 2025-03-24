459858e4bb8d598c90d627d217282ac22b0c69d7
PythonToGo <pythontogoplease@gmail.com>
2025-03-24 12:17:31 +0100

    feat: set admin

--- theatre/admin.py
+++ theatre/admin.py
@@ -4,9 +4,34 @@ from .models import (
     Performance, Reservation, Ticket
 )
 
+# ----------------------------
+# Set Inline
+# ----------------------------
+
+class ActorInline(admin.TabularInline):
+    model = Play.actors.through
+    extra = 1
+
+
+class GenreInline(admin.TabularInline):
+    model = Play.genres.through
+    extra = 1
+
+# ----------------------------
+# Registser PlayAdmin
+# ----------------------------
+
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    inlines = [ActorInline, GenreInline]
+    exclude = ('actors', 'genres')
+
+# ----------------------------
+# Register other models
+# ----------------------------
+
 admin.site.register(Actor)
 admin.site.register(Genre)
-admin.site.register(Play)
 admin.site.register(TheatreHall)
 admin.site.register(Performance)
 admin.site.register(Reservation)
